92e567cd262b09a4b994fc2478b65f285cbe193e
Jiaxin Shou <shoujiaxin@gmail.com>
2020-12-18 21:35:38 +0800

    feat: add the first demo

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jiaxin Shou on 2020/12/18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
