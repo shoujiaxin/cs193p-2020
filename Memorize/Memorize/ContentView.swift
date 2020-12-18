4a8c19860023f4aadfcf9990a562d71b589dc48c
Jiaxin Shou <shoujiaxin@gmail.com>
2020-12-18 22:28:25 +0800

    feat: finish lecture 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0 ..< 4) { _ in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                // Front
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                // Back
+                RoundedRectangle(cornerRadius: 10)
+                    .fill()
+            }
+        }
     }
 }
