052b20ac9d8a4ab277222b6bc6febb3604518db0
Jiaxin Shou <shoujiaxin@gmail.com>
2020-12-24 22:19:06 +0800

    feat: add SetGame project

---
+++ SetGame/SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Jiaxin Shou on 2020/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
